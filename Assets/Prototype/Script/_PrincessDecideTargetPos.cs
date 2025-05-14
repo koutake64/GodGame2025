dda0e962c3c9bb7d65d3c763689d13dae9f07731
古川龍聖 <stellamygithab@gmail.com>
2025-05-14 15:31:58 +0900

    お嬢様修正中

--- Assets/Prototype/Script/_PrincessDecideTargetPos.cs
+++ Assets/Prototype/Script/_PrincessDecideTargetPos.cs
@@ -16,6 +16,8 @@ public class _PrincessDecideTargetPos : MonoBehaviour
 
     private CharacterMoveController cmController;
 
+    private int key = -1;
+
     private void Start()
     {
         cmController = GetComponent<CharacterMoveController>();
@@ -86,88 +88,88 @@ public class _PrincessDecideTargetPos : MonoBehaviour
         // Dictionaryで配列を確保
         Dictionary<int, List<_FieldDataManager.S_FIELDINFO>> alignmentGroups = new Dictionary<int, List<_FieldDataManager.S_FIELDINFO>>();
 
-        // IDごとにグループ化
-        void GroupByAlignment(List<Vector2Int> positions)
-        {
-            foreach (var pos in positions)
-            {
-                var infoList = fdMng.GetInfoList(pos);
-                foreach (var info in infoList)
-                {
-                    // IDが入っていなければ次へ
-                    if (info.alignmentID == -1) continue;
+        //// IDごとにグループ化
+        //void GroupByAlignment(List<Vector2Int> positions)
+        //{
+        //    foreach (var pos in positions)
+        //    {
+        //        var infoList = fdMng.GetInfoList(pos);
+        //        foreach (var info in infoList)
+        //        {
+        //            // IDが入っていなければ次へ
+        //            if (info.alignmentID == -1) continue;
 
-                    // 一度もそのIDで配列確保されていなければIDの添字で配列確保
-                    if (!alignmentGroups.ContainsKey(info.alignmentID))
-                    {
-                        alignmentGroups[info.alignmentID] = new List<_FieldDataManager.S_FIELDINFO>();
-                    }
+        //            // 一度もそのIDで配列確保されていなければIDの添字で配列確保
+        //            if (!alignmentGroups.ContainsKey(info.alignmentID))
+        //            {
+        //                alignmentGroups[info.alignmentID] = new List<_FieldDataManager.S_FIELDINFO>();
+        //            }
 
-                    // IDの添字に情報追加
-                    alignmentGroups[info.alignmentID].Add(info);
-                }
-            }
-        }
+        //            // IDの添字に情報追加
+        //            alignmentGroups[info.alignmentID].Add(info);
+        //        }
+        //    }
+        //}
 
-        // IDごとに配列に格納
-        GroupByAlignment(wallPos);
-        GroupByAlignment(exhibitionStandPos);
+        //// IDごとに配列に格納
+        //GroupByAlignment(wallPos);
+        //GroupByAlignment(exhibitionStandPos);
 
-        foreach (var kv in alignmentGroups)
-        {
-            var group = kv.Value;
+        //foreach (var kv in alignmentGroups)
+        //{
+        //    var group = kv.Value;
 
-            // グループ内の要素数が2未満の場合は次へ
-            if (group.Count < 2) continue;
+        //    // グループ内の要素数が2未満の場合は次へ
+        //    if (group.Count < 2) continue;
 
-            // 方向が左右のどちらかならtrue
-            bool isHorizontal = group[0].dir == CommonSE_Proto.E_DIRECTION.right || group[0].dir == CommonSE_Proto.E_DIRECTION.left;
+        //    // 方向が左右のどちらかならtrue
+        //    bool isHorizontal = group[0].dir == CommonSE_Proto.E_DIRECTION.right || group[0].dir == CommonSE_Proto.E_DIRECTION.left;
 
-            // 方向に応じてソート
-            group.Sort((a, b) => isHorizontal ? a.pos.x.CompareTo(b.pos.x) : a.pos.y.CompareTo(b.pos.y));
+        //    // 方向に応じてソート
+        //    group.Sort((a, b) => isHorizontal ? a.pos.x.CompareTo(b.pos.x) : a.pos.y.CompareTo(b.pos.y));
 
-            // グループの端の座標を取得
-            List<Vector2Int> first = new List<Vector2Int>();
-            if (fdMng.GetIsThrough(group[0].pos + Vector2Int.up))
-                first.Add(group[0].pos + Vector2Int.up);
-            if (fdMng.GetIsThrough(group[0].pos + Vector2Int.right))
-                first.Add(group[0].pos + Vector2Int.right);
-            if (fdMng.GetIsThrough(group[0].pos + Vector2Int.down))
-                first.Add(group[0].pos + Vector2Int.down);
-            if (fdMng.GetIsThrough(group[0].pos + Vector2Int.left))
-                first.Add(group[0].pos + Vector2Int.left);
+        //    // グループの端の座標を取得
+        //    List<Vector2Int> first = new List<Vector2Int>();
+        //    if (fdMng.GetIsThrough(group[0].pos + Vector2Int.up))
+        //        first.Add(group[0].pos + Vector2Int.up);
+        //    if (fdMng.GetIsThrough(group[0].pos + Vector2Int.right))
+        //        first.Add(group[0].pos + Vector2Int.right);
+        //    if (fdMng.GetIsThrough(group[0].pos + Vector2Int.down))
+        //        first.Add(group[0].pos + Vector2Int.down);
+        //    if (fdMng.GetIsThrough(group[0].pos + Vector2Int.left))
+        //        first.Add(group[0].pos + Vector2Int.left);
 
-           List<Vector2Int> last = new List<Vector2Int>();
-            if (fdMng.GetIsThrough(group[^1].pos + Vector2Int.up))
-                first.Add(group[^1].pos + Vector2Int.up);
-            if (fdMng.GetIsThrough(group[^1].pos + Vector2Int.right))
-                first.Add(group[^1].pos + Vector2Int.right);
-            if (fdMng.GetIsThrough(group[^1].pos + Vector2Int.down))
-                first.Add(group[^1].pos + Vector2Int.down);
-            if (fdMng.GetIsThrough(group[^1].pos + Vector2Int.left))
-                first.Add(group[^1].pos + Vector2Int.left);
+        //   List<Vector2Int> last = new List<Vector2Int>();
+        //    if (fdMng.GetIsThrough(group[^1].pos + Vector2Int.up))
+        //        first.Add(group[^1].pos + Vector2Int.up);
+        //    if (fdMng.GetIsThrough(group[^1].pos + Vector2Int.right))
+        //        first.Add(group[^1].pos + Vector2Int.right);
+        //    if (fdMng.GetIsThrough(group[^1].pos + Vector2Int.down))
+        //        first.Add(group[^1].pos + Vector2Int.down);
+        //    if (fdMng.GetIsThrough(group[^1].pos + Vector2Int.left))
+        //        first.Add(group[^1].pos + Vector2Int.left);
 
-            for(int j = 0; j < first.Count; ++j)
-            {
-                for (int i = 0; i < last.Count; ++i)
-                {
-                    if (princessPos == first[j] && prevEdgeTargetPos != last[i])
-                    {
-                        if (j < 2)
-                        {
-                            prevTargetPos = nextTargetPos;
-                            nextTargetPos = last[j];
-                            prevEdgeTargetPos = first[j];
-                        }
-                        else
-                        {
-                            prevTargetPos = nextTargetPos;
-                            nextTargetPos = last[j];
-                            prevEdgeTargetPos = first[j];
-                        }
-                    }
-                }
-            }
+        //    for(int j = 0; j < first.Count; ++j)
+        //    {
+        //        for (int i = 0; i < last.Count; ++i)
+        //        {
+        //            if (princessPos == first[j] && prevEdgeTargetPos != last[i])
+        //            {
+        //                if (j < 2)
+        //                {
+        //                    prevTargetPos = nextTargetPos;
+        //                    nextTargetPos = last[j];
+        //                    prevEdgeTargetPos = first[j];
+        //                }
+        //                else
+        //                {
+        //                    prevTargetPos = nextTargetPos;
+        //                    nextTargetPos = last[j];
+        //                    prevEdgeTargetPos = first[j];
+        //                }
+        //            }
+        //        }
+        //    }
 
 
             //if (princessPos == first && last != prevEdgeTargetPos)
@@ -187,95 +189,91 @@ public class _PrincessDecideTargetPos : MonoBehaviour
             //    prevEdgeTargetPos = last;
             //    return;
             //}
-        }
+        //}
 
         // ターゲット候補の座標リストにゴール座標を追加
-        List<Vector2Int> candidatePosList = new List<Vector2Int>();
-        candidatePosList.Add(goalPos);
+        //List<Vector2Int> candidatePosDic = new List<Vector2Int>();
+        Dictionary<int, Vector2Int> candidatePosDic = new Dictionary<int, Vector2Int>();
+        candidatePosDic.Add(0, goalPos);
+        int cnt = 1;
 
         // 柱の周囲4箇所を移動候補リストに追加
         foreach (var pillar in pillarPos)
         {
             if (fdMng.GetIsThrough(pillar + Vector2Int.up))
             {
-                candidatePosList.Add(pillar + Vector2Int.up);
+                candidatePosDic.Add(cnt, pillar + Vector2Int.up);
             }
             if (fdMng.GetIsThrough(pillar + Vector2Int.right))
             {
-                candidatePosList.Add(pillar + Vector2Int.right);
+                candidatePosDic.Add(cnt, pillar + Vector2Int.right);
             }
             if (fdMng.GetIsThrough(pillar + Vector2Int.down))
             {
-                candidatePosList.Add(pillar + Vector2Int.down);
+                candidatePosDic.Add(cnt, pillar + Vector2Int.down);
             }
             if (fdMng.GetIsThrough(pillar + Vector2Int.left))
             {
-                candidatePosList.Add(pillar + Vector2Int.left);
+                candidatePosDic.Add(cnt, pillar + Vector2Int.left);
             }
+
+            cnt++;
+
         }
 
-        void AddAroundWithAlignmentCheck(List<Vector2Int> baseList)
-        {
-            foreach (var pos in baseList)
-            {
-                var infoList = fdMng.GetInfoList(pos);
-                foreach (var info in infoList)
-                {
-                    // IDがない場合は次へ
-                    if (info.alignmentID == -1) continue;
+        //void AddAroundWithAlignmentCheck(List<Vector2Int> baseList)
+        //{
+        //    foreach (var pos in baseList)
+        //    {
+        //        var infoList = fdMng.GetInfoList(pos);
+        //        foreach (var info in infoList)
+        //        {
+        //            // IDがない場合は次へ
+        //            if (info.alignmentID == -1) continue;
 
-                    // 指定座標の周囲4箇所を移動候補リストに追加
-                    if (fdMng.GetIsThrough(info.pos + Vector2Int.up))
-                    {
-                        candidatePosList.Add(info.pos + Vector2Int.up);
-                    }
-                    if (fdMng.GetIsThrough(info.pos + Vector2Int.right))
-                    {
-                        candidatePosList.Add(info.pos + Vector2Int.right);
-                    }
-                    if (fdMng.GetIsThrough(info.pos + Vector2Int.down))
-                    {
-                        candidatePosList.Add(info.pos + Vector2Int.down);
-                    }
-                    if (fdMng.GetIsThrough(info.pos + Vector2Int.left))
-                    {
-                        candidatePosList.Add(info.pos + Vector2Int.left);
-                    }
-                }
-            }
-        }
+        //            // 指定座標の周囲4箇所を移動候補リストに追加
+        //            if (fdMng.GetIsThrough(info.pos + Vector2Int.up))
+        //            {
+        //                candidatePosDic.Add(info.pos + Vector2Int.up);
+        //            }
+        //            if (fdMng.GetIsThrough(info.pos + Vector2Int.right))
+        //            {
+        //                candidatePosDic.Add(info.pos + Vector2Int.right);
+        //            }
+        //            if (fdMng.GetIsThrough(info.pos + Vector2Int.down))
+        //            {
+        //                candidatePosDic.Add(info.pos + Vector2Int.down);
+        //            }
+        //            if (fdMng.GetIsThrough(info.pos + Vector2Int.left))
+        //            {
+        //                candidatePosDic.Add(info.pos + Vector2Int.left);
+        //            }
+        //        }
+        //    }
+        //}
 
         // 壁と展示台の周囲の座標を移動候補リストに追加
-        AddAroundWithAlignmentCheck(wallPos);
-        AddAroundWithAlignmentCheck(exhibitionStandPos);
+        //AddAroundWithAlignmentCheck(wallPos);
+        //AddAroundWithAlignmentCheck(exhibitionStandPos);
 
         // 最適なターゲット座標を算出するための距離
         int minDistToPrincess = int.MaxValue;
         int minDistToGoal = int.MaxValue;
         Vector2Int bestTarget = princessPos;
 
-        int cnt = 0;
-        foreach (var pos in candidatePosList)
-        {
-            cnt++;
-            Debug.Log(
-                "座標 : " + pos + cnt + "個目"
-                );
-        }
-
         // 移動候補リストの中でプリンセスとゴールとの距離を計算しターゲットを決定
-        foreach (var pos in candidatePosList)
+        foreach (var pos in candidatePosDic)
         {
-            if (pos == princessPos)
+            if (pos.Value == princessPos || pos.Key == key)
             {
                 continue;
             }
 
             // プリンセスとの距離を計算
-            int distToPrincess = Mathf.Abs(pos.x - princessPos.x) + Mathf.Abs(pos.y - princessPos.y);
+            int distToPrincess = Mathf.Abs(pos.Value.x - princessPos.x) + Mathf.Abs(pos.Value.y - princessPos.y);
             
             // ゴールとの距離を計算
-            int distToGoal = Mathf.Abs(pos.x - goalPos.x) + Mathf.Abs(pos.y - goalPos.y);
+            int distToGoal = Mathf.Abs(pos.Value.x - goalPos.x) + Mathf.Abs(pos.Value.y - goalPos.y);
 
             // プリンセスとの距離が一番近い
             if (distToPrincess < minDistToPrincess ||
@@ -284,7 +282,8 @@ public class _PrincessDecideTargetPos : MonoBehaviour
             {
                 minDistToPrincess = distToPrincess;
                 minDistToGoal = distToGoal;
-                bestTarget = pos;
+                bestTarget = pos.Value;
+                key = pos.Key;
             }
         }
 
