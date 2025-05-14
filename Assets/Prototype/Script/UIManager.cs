40c78f5e9a0966335d6cfe6e5214e96bd8f3f9af
髙橋海翔 <kaito-t.2003@au.com>
2025-05-14 22:26:16 +0900

    UIManagerにコメント記載、UIはTimeScaleの影響を受けない設定に変更

--- Assets/Prototype/Script/UIManager.cs
+++ Assets/Prototype/Script/UIManager.cs
@@ -6,16 +6,29 @@ using UnityEngine.UI;
 /// </summary>
 public class UIManager : MonoBehaviour
 {
-    public Animator animator;
+    [Header("各UIオブジェクト")]
+    [SerializeField] private GameObject memoUIObj;
 
 
+    private Animator memoAnimator;  // メモUIアニメーター
 
-    private bool useMemo;
+    private bool useMemo;       // メモを開いているかどうか
+    private bool currentFlag;   // 現在のフラグ状況
+    private bool prevFlag;      // 1フレーム前のフラグ状況
 
 
-    private bool currentFlag;
-    private bool prevFlag;
-
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    private void Start()
+    {
+        // 各オブジェクトからAnimatorを取得＆nullチェック
+        memoAnimator = memoUIObj.GetComponent<Animator>();
+        if(!memoAnimator)
+        {
+            Debug.LogError("UI_MenoにAnimatorコンポーネントを追加してください。");
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,25 +36,50 @@ public class UIManager : MonoBehaviour
         InputUpdate();
         UpdateAnimator();
 
-
+        // 過去フラグ状況の更新
         prevFlag = currentFlag;
     }
 
 
-
+    /// <summary>
+    /// アニメーションの更新
+    /// </summary>
     void UpdateAnimator()
     {
-        animator.SetBool("isUseMemo", useMemo);
+        // TODO UIを追加したら随時ここに追加していく
+        memoAnimator.SetBool("isUseMemo", useMemo);
     }
 
-
+    /// <summary>
+    /// 入力ごとの更新
+    /// </summary>
     void InputUpdate()
     {
+        // 現在のフラグ状況を更新
         currentFlag = useMemo;
 
 
         // Tabキーでメモをポップアップする
         if (Input.GetKeyDown(KeyCode.Tab))
             useMemo = !useMemo;
+    }
+
+    /// <summary>
+    /// タイムスケール関連更新
+    /// </summary>
+    void UpdateTimeScale()
+    {
+        // ※AnimatorControllerの設定でUIアニメーションのTimeScaleは影響を受けない
+
+
+        // ここの条件はUIを開いている間、裏のゲーム自体を止めたい場合
+        if(useMemo)
+        {
+
+        }
+        else
+        {
+            // それ以外は裏で動いていても大丈夫
+        }
     }
 }
