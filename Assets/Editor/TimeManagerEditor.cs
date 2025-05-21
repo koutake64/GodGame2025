7850937f7e8948e71cbaa11cdabfdea250cf0c74
竹中 浩大朗 <tk0604777@gmail.com>
2025-05-21 17:19:28 +0900

    デバック機能削除

--- Assets/Editor/TimeManagerEditor.cs
+++ Assets/Editor/TimeManagerEditor.cs
@@ -4,33 +4,33 @@ using UnityEditor;
 [CustomEditor(typeof(TimeManager))]
 public class TimeManagerEditor : Editor
 {
-	public override void OnInspectorGUI()
-	{
-		DrawDefaultInspector();
+	//public override void OnInspectorGUI()
+	//{
+	//	DrawDefaultInspector();
 
-		TimeManager manager = (TimeManager)target;
+	//	TimeManager manager = (TimeManager)target;
 
-		GUILayout.Space(10);
-		GUILayout.Label("デバッグ用 時間帯切替", EditorStyles.boldLabel);
+	//	GUILayout.Space(10);
+	//	GUILayout.Label("デバッグ用 時間帯切替", EditorStyles.boldLabel);
 
-		if (GUILayout.Button("朝に切り替え"))
-		{
-			manager.ForceChangeState(CommonSE_Proto.E_TIMEOFDAY.morning);
-		}
+	//	if (GUILayout.Button("朝に切り替え"))
+	//	{
+	//		manager.ForceChangeState(CommonSE_Proto.E_TIMEOFDAY.morning);
+	//	}
 
-		if (GUILayout.Button("昼に切り替え"))
-		{
-			manager.ForceChangeState(CommonSE_Proto.E_TIMEOFDAY.noon);
-		}
+	//	if (GUILayout.Button("昼に切り替え"))
+	//	{
+	//		manager.ForceChangeState(CommonSE_Proto.E_TIMEOFDAY.noon);
+	//	}
 
-		if (GUILayout.Button("夕方に切り替え"))
-		{
-			manager.ForceChangeState(CommonSE_Proto.E_TIMEOFDAY.afternoon);
-		}
+	//	if (GUILayout.Button("夕方に切り替え"))
+	//	{
+	//		manager.ForceChangeState(CommonSE_Proto.E_TIMEOFDAY.afternoon);
+	//	}
 
-		if (GUILayout.Button("夜に切り替え"))
-		{
-			manager.ForceChangeState(CommonSE_Proto.E_TIMEOFDAY.night);
-		}
-	}
+	//	if (GUILayout.Button("夜に切り替え"))
+	//	{
+	//		manager.ForceChangeState(CommonSE_Proto.E_TIMEOFDAY.night);
+	//	}
+	//}
 }
