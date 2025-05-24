c8e95f11c074eba1b45da4db80a3eb92eed9b245
takeru_924 <n.tkr0924@gmail.com>
2025-05-24 23:26:10 +0900

    カメラが警備員を見つけたら斜め移動するバグ解決・CharacterMoceControllerの動きを止める処理を追加

--- Assets/Prototype/Script/SurveillanceCamera.cs
+++ Assets/Prototype/Script/SurveillanceCamera.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework.Internal.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
@@ -51,8 +52,10 @@ public class SurveillanceCamera : MonoBehaviour
     // マスの状態取得用
     //FieldDataManager fieldManager;
 
-    bool foundTarget = false;
-    bool prevFoundTarget = false;
+    // お嬢様呼び出し処理用
+    bool isFoundTarget = false;
+    int frameCount = 0;
+    List<SecurityController> callSecurityList = new List<SecurityController>();
 
     Vector2Int targetPos = new Vector2Int();
 
@@ -285,6 +288,9 @@ public class SurveillanceCamera : MonoBehaviour
     private void SearchRange()
     {
         int offsetValue = 0;
+        isFoundTarget = false;
+        frameCount++;
+
         // 状態に応じてスライド方向を決定
         if (forward == Vector2.right || forward == Vector2.left)
         {
@@ -327,7 +333,6 @@ public class SurveillanceCamera : MonoBehaviour
         // スライド方向を現在の向きに回転
         Vector2 slideDir = RotateOffset(new Vector2(offsetValue, 0), forward);
 
-
         // 索敵範囲の中心位置を計算（カメラの2マス先＋スライド方向）
         Vector2 center = SurveillanceCameraPos + forward * 2 + slideDir;
 
@@ -361,7 +366,6 @@ public class SurveillanceCamera : MonoBehaviour
 
             // ヒット順にソート（近い順）
             System.Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
-
 
             RaycastHit hit;
 
@@ -419,7 +423,8 @@ public class SurveillanceCamera : MonoBehaviour
                     if (hit.collider.CompareTag("Princess"))
                     {
                         Debug.Log($"プリンセス発見！: ({hitPos}) - {hit.collider.gameObject.name}");
-                        prevFoundTarget = foundTarget = true;
+                        isFoundTarget = true;
+                        frameCount = 0;
                         targetPos = hit.collider.GetComponent<CharacterMoveController>().GetCurrentPos();
                     }
                     else if (hit.collider.CompareTag("Player"))
@@ -427,11 +432,10 @@ public class SurveillanceCamera : MonoBehaviour
                         Debug.Log($"執事発見！: ({hitPos}) - {hit.collider.gameObject.name}");
                     }
                 }
-
             }
         }
 
-        if (foundTarget)
+        if (isFoundTarget)
         {
             // 呼び出し通知オブジェクトリスト
             List<SecurityController> securityObj = new List<SecurityController>();
@@ -478,72 +482,33 @@ public class SurveillanceCamera : MonoBehaviour
                             if (obj.GetIsFoundPrincess())
                                 continue;
 
+                            // 呼び出しオブジェクトリストに追加
+                            callSecurityList.Add(obj);
+
                             // ターゲット座標を通知
                             obj.FoundPrincess(targetPos);
+
+                            frameCount = 0;
                         }
                     }
                 }
             }
         }
-        // 前フレームと値が変化していたら
-        if (foundTarget != prevFoundTarget)
+
+        if(!isFoundTarget && frameCount > 120 && callSecurityList.Count != 0)
         {
-            // 呼び出し通知オブジェクトリスト
-            List<SecurityController> securityObj = new List<SecurityController>();
-
-            // 自身の座標
-            Vector2Int cameraPos = new Vector2Int((int)this.transform.position.x, (int)this.transform.position.z);
-
-            // 呼び始めの座標用
-            Vector2Int callStart = new Vector2Int(cameraPos.x - callRange / 2, cameraPos.y - callRange / 2);
-
-            // 通知範囲内にいる警備員を取得
-            for (int y = 0; y < callRange; ++y)
+            foreach(var security in callSecurityList)
             {
-                for (int x = 0; x < callRange; ++x)
+                if(security.GetIsFoundPrincess())
                 {
-                    // 範囲内のリスト取得
-                    Vector2Int callPos = new Vector2Int(callStart.x + x, callStart.y + y);
-
-                    // 範囲外確認
-                    if (callPos.x < 0 || callPos.x >= max.x || callPos.y < 0 || callPos.y >= max.y)
-                        continue;
-
-                    // 対象座標のリスト取得
-                    var list = fieldDataManager.GetInfoList(callPos);
-
-                    if (list.Count != 0)
-                    {
-                        // リストの中に警備員がいたら取得
-                        foreach (var obj in list)
-                        {
-                            if (!obj.obj) continue;
-                            var security = obj.obj.GetComponent<SecurityController>();
-                            if (!security) continue;
-
-                            // リストに追加
-                            securityObj.Add(security);
-                        }
-                    }
-                    if (securityObj.Count != 0)
-                    {
-                        foreach (var obj in securityObj)
-                        {
-                            // フラグが立っていなければ次へ
-                            if (!obj.GetIsFoundPrincess())
-                                continue;
-
-                            // お嬢さま発見フラグを下げる
-                            obj.SetIsFoundPrincess(false);
-                        }
-                    }
+                    security.SetIsFoundPrincess(false);
                 }
             }
+
+            // 配列を初期化
+            callSecurityList.Clear();
         }
-
-       
     }
-
 
     /// <summary>
     /// 前フレームに設定されたカメラの索敵範囲をリセット
