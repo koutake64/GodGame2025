3da4421517994f6937fc61a206a79b5922663711
髙橋海翔 <kaitogiotto@gmail.com>
2025-06-02 22:12:01 +0900

    時計UIの処理完成しました

--- Assets/Prototype/Script/TexUVMove.cs
+++ Assets/Prototype/Script/TexUVMove.cs
@@ -4,8 +4,6 @@ using UnityEngine.UI;
 public class TexUVMove : MonoBehaviour
 {
     [SerializeField] private RawImage image;
-    public float TexScaleX = 0.32f;
-    [SerializeField, Range(0.0f, 0.66f)] float UVX = 0.0f;
     [SerializeField, Header("切り替わる時間")] private float lerpTime = 1.0f;
 
 
@@ -13,22 +11,17 @@ public class TexUVMove : MonoBehaviour
 
     private float noonTimeStart;    // 昼前演出開始時間
     private float nightTimeStart;   // 夜前演出開始時間
-    private float curTime;          // 現在の時間
-    
+    private float gameTime;         // ゲーム内の時間
+    private float currenttime;      // 現在の時間
 
-    private Vector2 offset;
-    private Vector2[] StartUV =
-    {
-        new Vector2(0.0f / 3.0f, 0.0f),
-        new Vector2(1.0f / 3.0f, 0.0f),
-    };
-    private Vector2[] targetUV =
-    {
-        new Vector2(0.33f,0.0f),
-        new Vector2(0.66f,0.0f),
-    };
+    private Rect startRect;
+    private Rect targetRect;
+    private int currentIndex = 0;
+    private const int totalState = 3;   // 朝・昼・夜
 
     private bool isTransition;
+    private bool isSwitchedToNoon = false;
+    private bool isSwitchedToNight = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -40,41 +33,83 @@ public class TexUVMove : MonoBehaviour
                "TimeManagerがnullです");
         }
 
-        Material mat = Instantiate(image.material);
-        image.material = mat;
-
-        mat.mainTextureOffset = new Vector2(0.0f / 3.0f, 0.0f);
-        mat.mainTextureScale = new Vector2(0.32f, 1.0f);
-
+        // それぞれの演出開始時間をTimeManagerから取得する
         noonTimeStart = timeManager.GetTime(CommonSE_Proto.E_TIMEOFDAY.noon) - 5.0f;
         nightTimeStart = timeManager.GetTime(CommonSE_Proto.E_TIMEOFDAY.night) - 5.0f;
 
-        Debug.Log("noonStart:" + noonTimeStart);
-        Debug.Log("nightStart:" + nightTimeStart);
-
-        offset = image.material.mainTextureOffset;
+        // 最初は朝にしておく
+        SetUVIndex(0, true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        curTime = timeManager.GetCurrentTime();
+        // ゲーム中の時間を更新する
+        gameTime = timeManager.GetCurrentTime();
 
-        if (curTime >= noonTimeStart && !isTransition)
+        if(isTransition)
         {
-            isTransition = true;
-        }
+            currenttime += Time.deltaTime;
+            float t = Mathf.Clamp01(currenttime / lerpTime);
 
-        if (isTransition)
-        {
-            float t = Mathf.Clamp01(curTime / lerpTime);
-
-            offset.x = Mathf.Lerp(StartUV[0].x, targetUV[0].x, t);
-
-            image.material.mainTextureOffset = new Vector2(offset.x, 0.0f);
-
+            image.uvRect = LerpRect(startRect, targetRect, t);
             if (t >= 1.0f)
                 isTransition = false;
         }
+
+        // 昼前の演出開始
+        if (gameTime >= noonTimeStart && !isSwitchedToNoon)
+        {
+            // 昼に移行
+            SetUVIndex(1, false);
+            isSwitchedToNoon = true;
+        }
+
+        if(gameTime >= nightTimeStart && !isSwitchedToNight)
+        {
+            SetUVIndex(2, false);
+            isSwitchedToNight = true;
+        }
+    }
+
+    /// <summary>
+    /// UVの線形補間の値のセット
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="immdiate"></param>
+    void SetUVIndex(int index,bool immdiate)
+    {
+        currentIndex = index;
+        float uvWidth = 1.0f / (float)totalState;
+        Rect newRect = new Rect(index * uvWidth, 0f, uvWidth, 1f);
+
+        if(immdiate)
+        {
+            image.uvRect = newRect;
+            isTransition = false;
+        }
+        else
+        {
+            startRect = image.uvRect;
+            targetRect = newRect;
+            currenttime = 0f;
+            isTransition = true;
+        }
+    }
+
+    /// <summary>
+    /// Rect型の線形補間
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    Rect LerpRect(Rect from,Rect to,float t)
+    {
+        return new Rect(
+            Mathf.Lerp(from.x, to.x, t),
+            Mathf.Lerp(from.y, to.y, t),
+            Mathf.Lerp(from.width, to.width, t),
+            Mathf.Lerp(from.height, to.height, t));
     }
 }
