9ef69beb434c7c1f6b230532a31ce72be7cb44c1
WNHS20111\matsu <Peroo114514@gmail.com>
2025-06-05 12:35:41 +0900

    ゲームステージ床と場外床のベースを一致。１＊１ショウケースを使用してみた。⇒ガラスの色が濃すぎたのでマテリアル変更

--- Assets/Prototype/Script/_FieldDataManager.cs
+++ Assets/Prototype/Script/_FieldDataManager.cs
@@ -259,12 +259,12 @@ public class _FieldDataManager : MonoBehaviour
         }
 
         // --- 床外の床を生成
-        GameObject _outOfRangeTile1 = Instantiate(outOfRangeTile, new Vector3(-11, -0.5f, 7), Quaternion.identity);
-        _outOfRangeTile1.transform.localScale = new Vector3(21, 1, 15);
-        GameObject _outOfRangeTile2 = Instantiate(outOfRangeTile, new Vector3(0, -0.5f, -9), Quaternion.identity);
-        _outOfRangeTile2.transform.localScale = new Vector3(43, 1, 17);
-        GameObject _outOfRangeTile3 = Instantiate(outOfRangeTile, new Vector3(31, -0.5f, -1), Quaternion.identity);
-        _outOfRangeTile3.transform.localScale = new Vector3(21, 1, 33);
+        GameObject _outOfRangeTile1 = Instantiate(outOfRangeTile, new Vector3(-11, -0.51f, 0), Quaternion.identity);
+        _outOfRangeTile1.transform.localScale = new Vector3(70, 1, 33);
+        //GameObject _outOfRangeTile2 = Instantiate(outOfRangeTile, new Vector3(0, -0.5f, -9), Quaternion.identity);
+        //_outOfRangeTile2.transform.localScale = new Vector3(43, 1, 17);
+        //GameObject _outOfRangeTile3 = Instantiate(outOfRangeTile, new Vector3(31, -0.5f, -1), Quaternion.identity);
+        //_outOfRangeTile3.transform.localScale = new Vector3(21, 1, 33);
 
         // プロトタイプ用のステージ作成
         AddInfo(new Vector2Int(0, 1), E_FIELDSTATE.start, CommonSE_Proto.E_DIRECTION.right);
