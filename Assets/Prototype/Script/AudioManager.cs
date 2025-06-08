8a364c18cd0c941f4d909d689cf66eba070f86b4
Namielle1024 <xxgirudoxx@gmail.com>
2025-06-08 23:09:29 +0900

    オーディオ、インパクトエフェクト修正

--- Assets/Prototype/Script/AudioManager.cs
+++ Assets/Prototype/Script/AudioManager.cs
@@ -37,7 +37,6 @@ public class AudioManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
