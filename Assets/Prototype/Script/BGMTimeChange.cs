f8940a7be1d3646cbdc249b62195263c1c0ea04f
竹中 浩大朗 <tk0604777@gmail.com>
2025-06-16 15:12:15 +0900

    BGMを時間帯によって変更するように

---
+++ Assets/Prototype/Script/BGMTimeChange.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class BGMTimeManager : MonoBehaviour
+{
+    private TimeManager timeManager;
+    private CommonSE_Proto.E_TIMEOFDAY currentTimeState = CommonSE_Proto.E_TIMEOFDAY.morning;
+
+    private void Start()
+    {
+        timeManager = FindFirstObjectByType<TimeManager>();
+        if (timeManager == null)
+        {
+            Debug.LogError("TimeManager‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ");
+            return;
+        }
+
+        currentTimeState = timeManager.CurrentState;
+        PlayBGMByTime(currentTimeState);
+    }
+
+    private void Update()
+    {
+        if (timeManager == null) return;
+
+        var newState = timeManager.CurrentState;
+
+        if (newState != currentTimeState)
+        {
+            currentTimeState = newState;
+            PlayBGMByTime(newState);
+        }
+    }
+
+    private void PlayBGMByTime(CommonSE_Proto.E_TIMEOFDAY timeOfDay)
+    {
+        switch (timeOfDay)
+        {
+            case CommonSE_Proto.E_TIMEOFDAY.morning:
+                AudioManager.Instance.PlayBGM(0);
+                break;
+            case CommonSE_Proto.E_TIMEOFDAY.noon:
+                AudioManager.Instance.PlayBGM(1);
+                break;
+            case CommonSE_Proto.E_TIMEOFDAY.afternoon:
+                AudioManager.Instance.PlayBGM(1); 
+                break;
+            case CommonSE_Proto.E_TIMEOFDAY.night:
+                AudioManager.Instance.PlayBGM(2); 
+                break;
+        }
+    }
+}
